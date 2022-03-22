5f5eaf45b885a1f457f15151ce93a52ec31b5f30
Kári Þrastarson <kari.thrastarson@islandsbanki.is>
2022-03-22 21:50:33 +0000

    styles

--- public/js/main.js
+++ public/js/main.js
@@ -24,12 +24,10 @@ function toggleBurger() {
 
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
-    document.getElementById("hamburger").style.display = "none";
 }
 
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
-    document.getElementById("hamburger").style.display = "block";
 }
 
 function flip() {
