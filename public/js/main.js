5ce89bbf20eeea8004832c6233caabb12bf37ba3
Kári Þrastarson <kari.thrastarson@islandsbanki.is>
2023-02-27 16:53:57 +0000

    remove empty subpage

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,10 @@ $(document).ready(function() {
 /*Get div with id overlay*/
 var overlay = document.getElementById("overlay");
     $( "#overlay" ).append( $( "<a href=\"/\">VERKEFNIÐ</a>" ) );
-    $( "#overlay" ).append( $( "<a href=\"/\">TEYMIÐ</a>" ) );
     $( "#overlay" ).append( $( "<a href=\"/teikningar.html\">ÍBÚÐIR</a>" ) );
     $( "#overlay" ).append( $( "<a href=\"https://us14.list-manage.com/contact-form?u=3412f3b17965f94555bd17560&form_id=1a27fc054f26d25ffe397e4d0525be4f\">HAFA SAMBAND</a>" ) );
 
     $( "#topnav" ).append( $( "<a href=\"/\" class=\"nav\">VERKEFNIÐ</a>" ) );
-    $( "#topnav" ).append( $( "<a href=\"/\" class=\"nav\">TEYMIÐ</a>" ) );
     $( "#topnav" ).append( $( "<a href=\"/teikningar.html\" class=\"nav\">ÍBÚÐIR</a>" ) );
     $( "#topnav" ).append( $( "<a href=\"https://us14.list-manage.com/contact-form?u=3412f3b17965f94555bd17560&form_id=1a27fc054f26d25ffe397e4d0525be4f\"\n" +
         "       class=\"nav\">HAFÐU SAMBAND</a>" ) );
