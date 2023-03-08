9fa46f06b6ea3f4aa901a9c2d7586b47dedd605b
Kári Þrastarson <kari.thrastarson@islandsbanki.is>
2023-03-08 20:26:25 +0000

    Söluaðili

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,41 @@
 $(document).ready(function() {
+
+    /* Fetch query parameter to select real estate agent */
+    var sala = getUrlParameter('sala');
+    if (sala !== false){
+        localStorage.setItem("sala", sala);
+    }
+    var soluRedirect = localStorage.getItem("sala");
+    switch(soluRedirect) {
+        case "as":
+            $("#as").show();
+            $("#hraunhamar").hide();
+            $("#borg").hide();
+            break;
+        case "hraunhamar":
+            $("#hraunhamar").show();
+            $("#as").hide();
+            $("#borg").hide();
+            break;
+        case "borg":
+            $("#borg").show();
+            $("#as").hide();
+            $("#hraunhamar").hide();
+            break;
+        default:
+            $("#borg").show();
+            $("#as").show();
+            $("#hraunhamar").show();
+            break;
+    }
+
+    /*Remove query parameter from page*/
+    var uri = window.location.href.toString();
+    if (uri.indexOf("?") > 0) {
+        var clean_uri = uri.substring(0, uri.indexOf("?"));
+        window.history.replaceState({}, document.title, clean_uri);
+    }
+
     const buttons = document.querySelectorAll(".js-toggle-state");
 
     for (const button of buttons) {
@@ -10,18 +47,35 @@ $(document).ready(function() {
 
     $(".title").fadeIn(3000);
     $(".arrow").fadeIn(1000);
-  
-        function fnBlink() {
-      $(".arrow").fadeOut(1000);
-      $(".arrow").fadeIn(1000);
+
+    function fnBlink() {
+        $(".arrow").fadeOut(1000);
+        $(".arrow").fadeIn(1000);
     }
     setInterval(fnBlink, 2000);
 });
 $ (function () {
     $ ('#arrow') .click(function () {
-    $ ('html, body') .animate ({
-    scrollTop: $ ("#post") .offset().top - 0.25*$("#post")[0].scrollHeight
-    }, 750);
-    return false;
+        $ ('html, body') .animate ({
+            scrollTop: $ ("#post") .offset().top - 0.25*$("#post")[0].scrollHeight
+        }, 750);
+        return false;
     })
 });
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
