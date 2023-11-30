cf1ee93d33cdbf5da684ba75c02e07b73b468e01
Kári Þrastarson <kari.thrastarson@islandsbanki.is>
2023-11-30 20:35:46 +0000

    new release

--- public/js/index.js
+++ public/js/index.js
@@ -53,6 +53,8 @@ $(document).ready(function() {
     }
     setInterval(fnBlink, 2000);
     updateImageMap();
+
+    
 });
 $ (function () {
     $ ('#arrow') .click(function () {
@@ -81,10 +83,86 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 function updateImageMap() {
-    $.getJSON("new-image-map.json", function (mapdata) {
+    $.getJSON("image-map.json", function (mapdata) {
             ImageMapPro.init('#image-map-pro', mapdata);        
     }
     ).fail(function () {
         console.log("An error has occurred loading image map");
     });
 }
+
+$(window).on('resize scroll', function() {
+    if ($('.horizontal-scroller').isInViewport()) {
+        //CHeck if slide-left class is already applied
+        if(!$(".horizontal-scroller").hasClass("slide-left")) {
+            $(".horizontal-scroller").toggleClass("slide-left");
+        }
+        
+    } else {
+        // do something else
+    }
+});
+
+function toggleFullscreen(x) {
+
+    //If x has src attribute, set the src of the fullscreenview to x.src
+    if (x.hasAttribute('src')) {
+        $('.fullscreenview').css('background-image',  "url(" + x.src + ")");
+        $('.fullscreenview').css("display", "flex").hide().fadeIn(200);
+    }
+    else {
+        $('.fullscreenview').css("display", "none");
+    }
+}
+
+
+function updateImageMap() {
+    
+    $.getJSON("image-map.json", function (mapdata) {
+        initMap(mapdata);        
+    }
+    ).fail(function () {
+        console.log("An error has occurred loading image map");
+    });
+
+    ImageMapPro.subscribe((action) => {    
+        if (action.type === 'objectClick' && action.payload.object.length === 7) {
+            var houseNr = action.payload.object.slice(0, 2);
+            var aptNr = action.payload.object.slice(3, 7);
+                changeArtboard(houseNr);
+                revealApt(houseNr, aptNr);
+        }
+    });
+}
+
+function revealApt(houseNr, aptNr) {
+    //Set display block on apt-view
+    var href="upplysingar.html?hus="+houseNr+"&ibud="+aptNr+""
+    $("#front-page-apt-view img").attr('src', 'media/images/floor-plans/'+houseNr+'/'+aptNr+'.jpg');
+    $("#front-page-apt-view img").attr('onclick', "location.href='"+href+"'");
+    $("#front-page-apt-view-title").text("Áshamar "+houseNr+" - Íbúð "+aptNr);
+    $("#front-page-apt-view-button").attr('onClick', "location.href='" + href + "'");
+    $("#front-page-apt-view").removeClass("hide");
+
+    $ ('html, body') .animate ({
+        scrollTop: $ ("#front-page-apt-view") .offset().top - 0.25*$("#front-page-apt-view")[0].scrollHeight
+    }, 750);
+}
+
+function initMap(mapdata) {
+    ImageMapPro.init('#image-map-pro', mapdata);
+}   
+
+function changeArtboard(housenr) {
+    ImageMapPro.changeArtboard('ashamar-project','Áshamar '+housenr);
+}
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
